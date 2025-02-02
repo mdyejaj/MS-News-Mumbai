15c8ac0a372ef391001d2815d5b19a149ae4dbd5
mdyejajsah <mdyejajsah33@.com>
2025-02-02 16:14:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let menubar = document.querySelector('#menu-bars');
+let navbar  = document.querySelector('.navbar');
+
+menubar.onclick = () =>{
+    menubar.classList.toggle('fa-times');
+    navbar.classList.toggle('active')
+}
+
+// contact section started
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+// contact section ended
